1644e579a1e635fb7cbcf3b11c03bbfa522b3129
evoworld1 <evoworld1@gmail.com>
2022-05-21 18:10:58 +0200

    reorganizing project structure & apply webpack bundler

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const t=()=>{(()=>{const t=document.querySelector(".search-box");document.querySelector(".search-btn").addEventListener("click",(e=>{t.textContent=e.target.value;const c=t.textContent;console.log(c)}))})()};t(),t()})();
