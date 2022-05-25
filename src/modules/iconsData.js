41638a8c9b7a82a07fc11a94f1169bff87fa71bf
evoworld1 <evoworld1@gmail.com>
2022-05-25 08:11:20 +0200

    final commit

--- src/modules/iconsData.js
+++ src/modules/iconsData.js
@@ -1,18 +1,18 @@
 const arrayOfIcons = [
-  { tier: "IRON", src: "../../src/assets/Emblem_Iron.png" },
-  { tier: "BRONZE", src: "../../src/assets/Emblem_Bronze.png" },
-  { tier: "SILVER", src: "../../src/assets/Emblem_Silver.png" },
-  { tier: "GOLD", src: "../../src/assets/Emblem_Gold.png" },
-  { tier: "PLATINUM", src: "../../src/assets/Emblem_Platinum.png" },
-  { tier: "DIAMOND", src: "../../src/assets/Emblem_Diamond.png" },
-  { tier: "MASTER", src: "../../src/assets/Emblem_Master.png" },
+  { tier: "IRON", src: "../src/assets/Emblem_Iron.png" },
+  { tier: "BRONZE", src: "../src/assets/Emblem_Bronze.png" },
+  { tier: "SILVER", src: "../src/assets/Emblem_Silver.png" },
+  { tier: "GOLD", src: "../src/assets/Emblem_Gold.png" },
+  { tier: "PLATINUM", src: "../src/assets/Emblem_Platinum.png" },
+  { tier: "DIAMOND", src: "../src/assets/Emblem_Diamond.png" },
+  { tier: "MASTER", src: "../src/assets/Emblem_Master.png" },
   {
     tier: "GRANDMASTER",
-    src: "../../src/assets/Emblem_Grandmaster.png",
+    src: "../src/assets/Emblem_Grandmaster.png",
   },
   {
     tier: "CHALLENGER",
-    src: "../../src/assets/Emblem_Challenger.png",
+    src: "../src/assets/Emblem_Challenger.png",
   },
 ];
 
