ea3da294f6541bb91e7f18d31b32058b866477fa
evoworld1 <evoworld1@gmail.com>
2022-05-25 09:27:22 +0200

    Regenerate API key

--- src/modules/fetchApiData.js
+++ src/modules/fetchApiData.js
@@ -2,7 +2,7 @@ import appendDataToDOM from "./appendApiData";
 import { summonersName } from "./eventHandlers";
 
 const fetchApiData = async () => {
-  const api_key = "api_key=RGAPI-83039753-b089-4804-8025-d83281bb6744";
+  const api_key = "api_key=RGAPI-a43655ea-02f9-4a16-a162-7fa2f1665c61";
 
   let getPlayerDataBySummonersName;
   let playerEncryptedSummonerId;
